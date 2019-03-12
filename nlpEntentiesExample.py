92c0cdc795b0e53e3b631675d777a37472874f02
OtacilioN <otacilio.n97@gmail.com>
2019-03-12 16:08:27 -0300

    Fix phrase at nlpEntenties

--- nlpEntentiesExample.py
+++ nlpEntentiesExample.py
@@ -3,7 +3,7 @@ utf8stdout = open(1, 'w', encoding='utf-8',
                   closefd=False)
 
 nlp = spacy.load('pt')
-doc = nlp('Silvio Meira uma vez disse que o Porto Digital que fica em Recife')
+doc = nlp('Silvio Meira uma vez disse que o Porto Digital fica em Recife')
 
 for ent in doc.ents:
     print(ent.text, ent.start_char, ent.end_char, ent.label_)
